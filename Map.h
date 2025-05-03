3ef83044b239d57f3652b73463645bc103dfae88
Nhihtcode <daoducthinh112@gmail.com>
2025-05-03 16:32:04 +0700

    Update Map.h

--- Map.h
+++ Map.h
@@ -62,31 +62,32 @@ void loadMapFromFile(const std::string& filePath) {
     file.close();
 }
 
-    void render(RenderWindow& window, const Texture& tileSet) {
-        Sprite tile;
-        tile.setTexture(tileSet);
-        auto& currentMap = getMap(0);
-    
-        for (int i = 0; i < _height; i++) {
-            for (int j = 0; j < _width; j++) {
-                char tileChar = currentMap[i][j];
-                if (tileChar == ' ' || tileChar == '0') continue;
-    
-                if (tileChar == 'P') tile.setTextureRect(IntRect(143 - 16 * 3, 112, 16, 16));   //Gạch nền thường (brick)
-                else if (tileChar == 'k') tile.setTextureRect(IntRect(143, 112, 16, 16));       //gạch tường thành
-                else if (tileChar == 'c') tile.setTextureRect(IntRect(143 - 16, 112, 16, 16));  //"?"
-                else if (tileChar == 't') tile.setTextureRect(IntRect(0, 47, 32, 48));          //Ống nước (ống đứng, 2 block)
-                else if (tileChar == 'g') tile.setTextureRect(IntRect(0, 139, 48, 37));         //Bụi cây nhỏ / thấp
-                else if (tileChar == 'G') tile.setTextureRect(IntRect(145, 222, 77, 33));       //Bụi cây lớn / rậm
-                else if (tileChar == 'd') tile.setTextureRect(IntRect(0, 106, 74, 21));         //bụi cây
-                else if (tileChar == 'w') tile.setTextureRect(IntRect(99, 224, 41, 31));        //Mây
-                else if (tileChar == 'r') tile.setTextureRect(IntRect(143 - 32, 112, 16, 16));  //gạch nâu
-    
-                tile.setPosition(j * 16 - offsetX, i * 16 - offsetY);
-                window.draw(tile);
-            }
+void render(RenderWindow& window, const Texture& tileSet) {
+    Sprite tile;
+    tile.setTexture(tileSet);
+    auto& currentMap = getMap(0);
+
+    for (int i = 0; i < _height; i++) {
+        for (int j = 0; j < _width; j++) {
+            char tileChar = currentMap[i][j];
+            if (tileChar == ' ' || tileChar == '0') continue;
+
+            if (tileChar == 'P') tile.setTextureRect(IntRect(143 - 16 * 3, 112, 16, 16));   //Gạch nền thường (brick)
+            else if (tileChar == 'k') tile.setTextureRect(IntRect(143, 112, 16, 16));       //gạch tường thành
+            else if (tileChar == 'c') tile.setTextureRect(IntRect(143 - 16, 112, 16, 16));  //"?"
+            else if (tileChar == 't') tile.setTextureRect(IntRect(0, 47, 32, 48));          //Ống nước (ống đứng, 2 block)
+            else if (tileChar == 'g') tile.setTextureRect(IntRect(0, 139, 48, 37));         //Bụi cây nhỏ / thấp
+            else if (tileChar == 'G') tile.setTextureRect(IntRect(145, 222, 77, 33));       //Bụi cây lớn / rậm
+            else if (tileChar == 'd') tile.setTextureRect(IntRect(0, 106, 74, 21));         //bụi cây
+            else if (tileChar == 'w') tile.setTextureRect(IntRect(99, 224, 41, 31));        //Mây
+            else if (tileChar == 'r') tile.setTextureRect(IntRect(143 - 32, 112, 16, 16));  //gạch nâu
+            else continue; // Nếu không phải các loại tile đã định nghĩa, bỏ qua
+            
+            tile.setPosition(j * 16 - offsetX, i * 16 - offsetY);
+            window.draw(tile);
         }
     }
+}
 };
 
 #endif // _MAP_H_
